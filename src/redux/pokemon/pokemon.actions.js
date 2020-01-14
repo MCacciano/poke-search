52034204d4dab6cf83773ba025056a0a466771b9
mcacciano <michael.cacciano@gmail.com>
2020-01-14 01:06:35 -0500

    eod working on infinite scroll for pagination

--- src/redux/pokemon/pokemon.actions.js
+++ src/redux/pokemon/pokemon.actions.js
@@ -2,9 +2,19 @@ import pokeApi from '../../axios/pokeApi';
 import { GET_POKEMON, GET_POKEMON_DETAILS } from '../types';
 
 // get all pokemon
-export const getAllPokemon = () => async dispatch => {
+export const getAllPokemon = (
+  more = false,
+  prev = 40,
+  limit = 40
+) => async dispatch => {
   try {
-    const res = await pokeApi.get('/pokemon?limit=151');
+    let res;
+
+    if (more) {
+      const res = await pokeApi.get(`/pokemon?previous=${prev}&limit=${limit}`);
+    } else {
+      res = await pokeApi.get(`/pokemon?previous=${prev + 40}&limit=${limit}`);
+    }
 
     dispatch({
       type: GET_POKEMON,
